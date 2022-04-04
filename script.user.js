0b231021e70d2e8c94161500a12afe6ba9e76f0c
ca-place <102911623+ca-place@users.noreply.github.com>
2022-04-03 20:08:07 -0400

    Update script.user.js
    Up version for bug fix

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canada Bot
 // @namespace
-// @version      2.0
+// @version      2.0.1
 // @description  For Canada!
 // @author       Canada Place Project
 // @match        https://www.reddit.com/r/place/*
